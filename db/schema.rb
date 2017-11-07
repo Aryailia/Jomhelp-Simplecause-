6248c2811d6397dd4e82b624410fe63c2e3b5569
zyang95 <leetzyyang@gmail.com>
2017-11-07 13:44:31 +0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107032349) do
+ActiveRecord::Schema.define(version: 20171107040825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contributors", force: :cascade do |t|
+    t.integer "charity_id", null: false
+    t.integer "user_id", null: false
+    t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "organisations", force: :cascade do |t|
     t.string "name"
