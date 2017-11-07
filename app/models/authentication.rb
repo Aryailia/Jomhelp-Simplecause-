62e94b70d16476f06d0293e3857f5d13ae34aefd
Aryailia <Aryailia@users.noreply.github.com>
2017-11-08 04:31:18 +0800

    changing navbar

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -2,15 +2,15 @@ class Authentication < ApplicationRecord
   belongs_to :user
 
 	def self.create_with_omniauth(auth_hash)
-       self.new(
-         provider: auth_hash["provider"],
-         uid: auth_hash["uid"],
-         token: auth_hash["credentials"]["token"]
-       )
+    self.new(
+      provider: auth_hash["provider"],
+      uid: auth_hash["uid"],
+      token: auth_hash["credentials"]["token"]
+    )
 	end
 
 	def update_token(auth_hash)
-       self.token = auth_hash["credentials"]["token"]
-       self.save
-    end
+    self.token = auth_hash["credentials"]["token"]
+    self.save
+  end
 end
