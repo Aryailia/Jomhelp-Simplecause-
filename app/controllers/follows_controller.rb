0ae7e1cdedb8f085d58911fcb5c41a826e5a19cc
jesontheg6 <jesonleejunzhen@gmail.com>
2017-11-07 13:55:26 +0800

    created follows

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,11 @@
+class FollowsController < ApplicationController
+
+def follow_user
+    @user = User.find_by! user_name: params[:user_name]
+end 
+
+def unfollow_user 
+
+	@user = User.find_by! user_name: params[:user_name]
+    if current_user.unfollow @user.id
+end
