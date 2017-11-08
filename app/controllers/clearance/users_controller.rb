1213d67fb86574aca43ea8b9802c9ede03f5e1b8
Aryailia <Aryailia@users.noreply.github.com>
2017-11-08 16:27:44 +0800

    Update users_controller.rb

--- app/controllers/clearance/users_controller.rb
+++ app/controllers/clearance/users_controller.rb
@@ -59,12 +59,4 @@ class Clearance::UsersController < Clearance::BaseController
     params[Clearance.configuration.user_parameter] || Hash.new
   end
 
-  def follow(user_id)
-    following_relationships.create(following_id: user_id)
-  end
-
- def unfollow(user_id)
-    following_relationships.find_by(following_id: user_id).destroy
-  end
-
 end
