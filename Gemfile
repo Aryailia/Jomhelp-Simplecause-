72acf9a832bd8ec819ac2dfa5d0f6c6df7187de2
zyang95 <leetzyyang@gmail.com>
2017-11-10 20:33:36 +0800

    css summore for so many pages

--- Gemfile
+++ Gemfile
@@ -32,7 +32,6 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'will_paginate', '~> 3.1.0'
-gem 'install faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def events(organisation) 
 		@events = organisation.events
-		@past_events = @events.where("end_date < ?", Time.now)
-		@upcoming_events = @events.where("end_date >", Time.now)
+		@past_events = @events.where("end_date <= ?", Time.now)
+		@upcoming_events = @events.where("end_date >= ?", Time.now)
 	end
 end
