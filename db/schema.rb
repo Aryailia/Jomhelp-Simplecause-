d851cc5811f8d137a791800171bb6e331b3a2a4d
Aryailia <Aryailia@users.noreply.github.com>
2017-11-13 22:02:29 +0800

    iunno stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113025127) do
+ActiveRecord::Schema.define(version: 20171113025126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,9 @@ ActiveRecord::Schema.define(version: 20171113025127) do
     t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "event_created", default: false
+    t.boolean "event_join", default: false
+    t.boolean "organisation_post", default: false
     t.index ["organisation_id"], name: "index_posts_on_organisation_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
