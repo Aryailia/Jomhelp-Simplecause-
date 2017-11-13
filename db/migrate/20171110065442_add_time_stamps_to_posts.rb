20c238044b3e687076649aa96b4144bbcb8f1dfa
jesontheg6 <jesonleejunzhen@gmail.com>
2017-11-13 18:16:53 +0800

    added 'organisation that i followed' in profile

---
+++ db/migrate/20171110065442_add_time_stamps_to_posts.rb
@@ -0,0 +1,6 @@
+class AddTimeStampsToPosts < ActiveRecord::Migration[5.1]
+  def change
+  	add_column :posts, :created_at, :datetime,  default: Time.zone.now, null: false
+  	add_column :posts, :updated_at, :datetime,  default: Time.zone.now, null: false
+  end
+end
